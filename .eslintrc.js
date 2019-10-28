68421b5bef87aa6be18060ab494ffc8b65771618
pamigit <pamizoki@gmail.com>
2019-10-28 22:48:21 +0100

    init commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,13 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    "@vue/prettier",
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint"
   },
 };
